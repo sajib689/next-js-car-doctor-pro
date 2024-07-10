f7660cee94c836fbb21e1874208dfcee7108638b
sajib689 <jibonraja11@gmail.com>
2024-07-10 12:12:30 +0600

    add banner

--- src/app/components/HomePage/Banner.jsx
+++ src/app/components/HomePage/Banner.jsx
@@ -1,27 +1,55 @@
-
-
 const Banner = () => {
-    return (
-        <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-        <div>
-        <h1>Hello</h1>
-        </div>
+  return (
+    <div className="carousel w-full">
+      {banners.map((banner, index) => (
+        <div
+        style={{
+          background: `linear-gradient(45deg, rgba(7,25,82,.7), rgba(0,0,0,0.3)), url(/assets/images/banner/${index+1}.jpg)`
+        }} key={index} id="slide1" className="carousel-item relative w-full h-screen">
+          <div>
+            <h1>{banner.title}</h1>
+          </div>
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide4" className="btn btn-circle">❮</a>
-            <a href="#slide2" className="btn btn-circle">❯</a>
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
           </div>
         </div>
-       
-      </div>
-    );
+      ))}
+    </div>
+  );
 };
 const banners = [
-    {
-        title: 'Affordable Price For Car Servicing',
-        description: 'There are many variations of passages of  available, but the majority have suffered alteration in some form',
-        next: '#slide2',
-        prev: '#slide4'
-    }
-]
+  {
+    title: "Affordable Price For Car Servicing",
+    description:
+      "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+    next: "#slide2",
+    prev: "#slide4",
+  },
+  {
+    title: "Affordable Price For Car Servicing",
+    description:
+      "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+    next: "#slide3",
+    prev: "#slide1",
+  },
+  {
+    title: "Affordable Price For Car Servicing",
+    description:
+      "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+    next: "#slide4",
+    prev: "#slide2",
+  },
+  {
+    title: "Affordable Price For Car Servicing",
+    description:
+      "There are many variations of passages of  available, but the majority have suffered alteration in some form",
+    next: "#slide1",
+    prev: "#slide3",
+  },
+];
 export default Banner;
