b463b63679e276a7dae095560ad09dd5f53efca0
sajib689 <jibonraja11@gmail.com>
2024-07-10 12:16:04 +0600

    fix an error

--- src/app/components/HomePage/Banner.jsx
+++ src/app/components/HomePage/Banner.jsx
@@ -5,15 +5,15 @@ const Banner = () => {
         <div
         style={{
           background: `linear-gradient(45deg, rgba(7,25,82,.7), rgba(0,0,0,0.3)), url(/assets/images/banner/${index+1}.jpg)`
-        }} key={index} id="slide1" className="carousel-item relative w-full h-screen">
+        }} key={index} id={`slide${index+1}`} className="carousel-item relative w-full h-screen">
           <div>
             <h1>{banner.title}</h1>
           </div>
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide4" className="btn btn-circle">
+            <a href={banner.prev} className="btn btn-circle">
               ❮
             </a>
-            <a href="#slide2" className="btn btn-circle">
+            <a href={banner.next} className="btn btn-circle">
               ❯
             </a>
           </div>
