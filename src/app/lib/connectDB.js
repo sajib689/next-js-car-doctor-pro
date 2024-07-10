a900b0ff43e028a7bce83aa49c1611b5c4723318
sajib689 <jibonraja11@gmail.com>
2024-07-10 19:32:14 +0600

    add mongodb

--- src/app/lib/connectDB.js
+++ src/app/lib/connectDB.js
@@ -1,3 +1,18 @@
+let db
 export const connectDB = async () => {
-    
+ if(db) return db
+ try {
+    const uri = process.env.NEXT_PUBLIC_MONGODB_URI
+    const client = new MongoClient(uri, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        }
+      });
+      db = client.db('car-doctors');
+      return db
+ } catch (err) {
+    console.log(err)
+ }
 }
