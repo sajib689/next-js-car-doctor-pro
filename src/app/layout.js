3c45b2ff6dbc60bfed6256babcffa73f228dbb8e
sajib689 <jibonraja11@gmail.com>
2024-07-10 19:33:54 +0600

    add mongodb connection with compas

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Shared/Navbar";
 import Footer from "./components/Shared/Footer";
+import AuthProvider from "@/Services/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme='carDoctorTheme'>
-      <body className={inter.className}>
+     <AuthProvider>
+     <body className={inter.className}>
         <Navbar/>
         {children}
         <Footer/>
         </body>
+     </AuthProvider>
     </html>
   );
 }
