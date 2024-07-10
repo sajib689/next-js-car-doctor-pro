325130f0379d1b9001ffc91c2654b31b7a49c680
sajib689 <jibonraja11@gmail.com>
2024-07-10 18:13:14 +0600

    try to connect on database

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,9 +1,13 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import { FaGoogle,FaFacebookSquare  } from "react-icons/fa";
 import { IoLogoGithub } from "react-icons/io";
 
 const page = () => {
+    const handleSignUp = () => {
+        console.log("Login")
+    }
   return (
     <div className="container mx-auto py-24">
       <div className="grid grid-cols-2 gap-12">
@@ -17,7 +21,7 @@ const page = () => {
         </div>
         <div className=" p-12">
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form onSubmit={handleSignUp} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name</span>
